b517ac286676805ebf07fb0a9d8428e71c8dcc36
Kolesnyk Oleksandr <kolesnyk.oleksandr.2004@gmail.com>
2025-05-09 15:35:59 +0300

    Added stacked header, added CRUD operations through dialogs

--- HomeownersManagmentApp/Models/ReportModel.cs
+++ HomeownersManagmentApp/Models/ReportModel.cs
@@ -4,7 +4,7 @@
     {
         public int ReportId { get; set; }
         public string ReportName { get; set; }
-        public List<Column> Columns { get; set; }
+        public List<ColumnGroup> ColumnGroups { get; set; }
 
         public List<string> ColumnsString
         {
@@ -12,14 +12,23 @@
             {
                 List<string> list = new List<string>();
 
-                foreach (var column in Columns)
+                foreach (var columnGroup in ColumnGroups)
                 {
-                    list.Add(column.name);
+                    foreach (var column in columnGroup.Columns)
+                    {
+                        list.Add(column.name);
+                    }
                 }
 
                 return list;
             }
         }
+    }
+
+    public class ColumnGroup
+    {
+        public string Name { get; set; }
+        public List<Column> Columns { get; set; }
     }
 
     public class Column
@@ -30,6 +39,5 @@
         public string align { get; set; }
         public string format { get; set; }
         public bool isGroupBy { get; set; }
-        public string Group { get; set; }
     }
 }
--- HomeownersManagmentApp/Models/View.cs
+++ HomeownersManagmentApp/Models/View.cs
@@ -2,8 +2,17 @@
 {
     public class View
     {
-        public Guid Id { get; set; }
-        public string Name { get; set; }
-        public List<string> Settings { get; set; }
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string Name { get; set; } = "";
+        public List<string> Settings { get; set; } = new List<string>();
+
+        public View() { }
+
+        public View(View view)
+        {
+            Id = view.Id;
+            Name = view.Name;
+            Settings = view.Settings;
+        }
     }
 }
